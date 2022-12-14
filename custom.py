fcf6248016240ff608768d076517f4d41cc4e839
dinhngocduong <dinhngocduong@luvina.net>
2022-12-14 13:16:35 +0700

    123456

--- custom.py
+++ custom.py
@@ -1,6 +1,8 @@
 import sys
+import pre_commit
 print("Hello from custom.py")  # Stdout only printed when Failed
 try:
     1 / 0
 except Exception as e:
     sys.exit(e)
+    print("Hello from custom.py v Hello from custom.py Hello from custom.py Hello from custom.py Hello from custom.py Hello from custom.py Hello from custom.py")
